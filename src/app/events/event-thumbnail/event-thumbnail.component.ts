159ee72dc6d37846f221db58ccc3b7b10efb7eb6
Álvaro Martínez-Tizón García <amartineztizon@gmail.com>
2023-04-01 20:46:27 +0200

    Styling in the ts

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -3,11 +3,17 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'event-thumbnail',
   templateUrl: './event-thumbnail.component.html',
+  styles: [
+    `
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well div {
+        color: #bbb;
+      }
+    `,
+  ],
 })
 export class EventThumbnailComponent {
   @Input() event: any;
-  someProperty: any = 'some value'
-  logFoo() {
-    console.log('foo');
-  }
 }
