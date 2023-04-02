9474f8631969193316ee663a6c8783fdae3a7960
Álvaro Martínez-Tizón García <amartineztizon@gmail.com>
2023-04-02 12:06:48 +0200

    Structural directive *ngFor

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -5,6 +5,9 @@ import { Component, Input } from '@angular/core';
   templateUrl: './event-thumbnail.component.html',
   styles: [
     `
+      .thumbnail {
+        min-height: 210px;
+      }
       .pad-left {
         margin-left: 10px;
       }
