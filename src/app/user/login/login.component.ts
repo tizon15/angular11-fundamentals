3ac5fd477c81b5102c14ad59f5e507f2d419451c
Álvaro Martínez-Tizón García <amartineztizon@gmail.com>
2023-04-04 13:29:48 +0200

    Validating Template-based Forms

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -5,10 +5,20 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+    `,
+  ],
 })
 export class LoginComponent implements OnInit {
   userName: string;
   password: string;
+  mouseoverLogin
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
