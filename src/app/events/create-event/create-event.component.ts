fb2397a0aa027028ebf2710b6d9f4c9fc6508394
Álvaro Martínez-Tizón García <amartineztizon@gmail.com>
2023-04-05 12:38:30 +0200

    Editing Data with Two-way Bindings

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -29,16 +29,30 @@ import { EventService } from '../shared/index';
 })
 export class CreateEventComponent implements OnInit {
   isDirty = true;
-  newEvent;
+  event: any;
   constructor(private router: Router, private eventService: EventService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.event = {
+      id: 1,
+      name: 'Angular Connect',
+      date: new Date('9/26/2036'),
+      time: '10:00 am',
+      price: 599.99,
+      location: {
+        address: '1057 DT',
+        city: 'London',
+        country: 'England',
+      },
+      onlineUrl: 'http://ngSpectacular.com',
+      imageUrl: 'http://ngSpectacular.com/logo.png'
+    };
+  }
 
   saveEvent(formValues) {
     this.eventService.saveEvent(formValues);
     this.isDirty = false;
     this.router.navigate(['/events']);
-
   }
   cancel(): void {
     this.router.navigate(['/events']);
