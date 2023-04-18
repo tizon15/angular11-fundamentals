79235cbd355563503bf85dc830faae0c1c409ad9
Álvaro Martínez-Tizón García <amartineztizon@gmail.com>
2023-04-18 12:40:20 +0200

    Filtering Data

--- src/app/events/events-details/event-details.component.ts
+++ src/app/events/events-details/event-details.component.ts
@@ -23,6 +23,7 @@ import { IEvent, ISession } from '../shared';
 export class EventDetailsComponent implements OnInit {
   event: IEvent;
   addMode: boolean;
+  filterBy = 'all';
 
   constructor(
     private eventService: EventService,
--- src/app/events/events-details/sessions-list/sessions-list.component.ts
+++ src/app/events/events-details/sessions-list/sessions-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ISession } from '../../shared';
 
 @Component({
@@ -6,8 +6,25 @@ import { ISession } from '../../shared';
   templateUrl: './sessions-list.component.html',
   styles: [],
 })
-export class SessionsListComponent implements OnInit {
+export class SessionsListComponent implements OnInit, OnChanges {
     @Input() sessions: ISession[];
+    @Input() filterBy: string;
+    visibleSessions: ISession[] = [];
+
   constructor() {}
   ngOnInit(): void {}
+  ngOnChanges(): void {
+    if(this.sessions){
+      this.filterSessions(this.filterBy);
+    }
+  }
+  filterSessions(filter: string){
+     if(filter === 'all'){
+      this.visibleSessions = this.sessions.slice(0)
+     } else {
+      this.visibleSessions = this.sessions.filter(session =>{
+        return session.level.toLocaleLowerCase() === filter;
+      })
+     }
+  }
 }
