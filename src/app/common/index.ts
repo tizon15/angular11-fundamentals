514a897fe3191a2aa728707f2aa7cd3403a5bb65
Álvaro Martínez-Tizón García <amartineztizon@gmail.com>
2023-04-19 11:50:03 +0200

    Creating a modal component

--- src/app/common/index.ts
+++ src/app/common/index.ts
@@ -1,3 +1,4 @@
 export * from './collapse-well/collapse-well.component'
 export * from './jquery/jQuery.service'
 export * from './toastr/toastr.service'
+export * from './simple-modal/simple-modal.component'
